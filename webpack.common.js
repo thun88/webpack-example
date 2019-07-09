53d2702fae09a319d49ce4e76c0ac8fe7fcd9d49
wlsun <wlsun@gruppocdm.it>
2019-07-09 17:17:09 +0200

    Installed  image-webpack-loader  for a better images optimised size

--- webpack.common.js
+++ webpack.common.js
@@ -45,10 +45,34 @@ module.exports = {
       ]
     },
     {
-      test: /\.(png|svg|jpg|gif)$/,
+      test: /\.(gif|png|jpe?g|svg)$/i,
       use: [
-        'file-loader'
-      ]
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+              quality: 65
+            },
+            // optipng.enabled: false will disable optipng
+            optipng: {
+              enabled: true,
+            },
+            pngquant: {
+              quality: '65-90',
+              speed: 4
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            // the webp option will enable WEBP
+            webp: {
+              quality: 75
+            }
+          }
+        },
+      ],
     },
     {
       test: /\.(woff|woff2|eot|ttf|otf)$/,
